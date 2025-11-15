af5b4f42b7641e7442a6daf0aacc9316f6f45777
Thomas Parker <32755663+tparker48@users.noreply.github.com>
2025-11-15 13:22:57 -0800

    add and / andi tests

--- tests/emulator_tests/instructions/and.s
+++ tests/emulator_tests/instructions/and.s
@@ -1,1 +1,60 @@
-# TODO
+li $t0, 0xFFFFFFFF
+li $t1, 0x0000000
+jal and_print
+nop
+
+li $t0, 0x00000000
+li $t1, 0xFFFFFFFF
+jal and_print
+nop
+
+li $t0, 0xF0F0F0F0
+li $t1, 0x0F0F0F0F
+jal and_print
+nop
+
+li $t0, 0x0F0F0F0F
+li $t1, 0xF0F0F0F0
+jal and_print
+nop
+
+li $t0, 0xAAAAAAAA
+li $t1, 0x55555555
+jal and_print
+nop
+
+li $t0, 0xFFFFFFFF
+li $t1, 0x55555555
+jal and_print
+nop
+
+li $t0, 0xFFFFFFFF
+li $t1, 0xAAAAAAAA
+jal and_print
+nop
+
+li $t0, 0xFFFFFFFF
+li $t1, 0xFFFFFFFF
+jal and_print
+nop
+
+j exit
+nop
+
+
+and_print:
+li $v0, 1
+and $a0, $t0, $t1
+syscall
+
+li $v0, 11
+li $a0, 10
+syscall
+
+jr $ra
+nop
+
+exit:
+li $v0, 10
+li $a0, 0
+syscall
--- tests/emulator_tests/instructions/andi.s
+++ tests/emulator_tests/instructions/andi.s
@@ -1,1 +1,59 @@
-# TODO
+li $t0, 0xFFFFFFFF
+andi $a0, $t0, 0x000
+jal print_a0
+nop
+
+li $t0, 0x00000000
+andi $a0, $t0, 0xFFFF
+jal print_a0
+nop
+
+li $t0, 0xF0F0F0F0
+andi $a0, $t0, 0x0F0F
+jal print_a0
+nop
+
+li $t0, 0x0F0F0F0F
+andi $a0, $t0, 0xF0F0
+jal print_a0
+nop
+
+li $t0, 0xAAAAAAAA
+andi $a0, $t0, 0x5555
+jal print_a0
+nop
+
+li $t0, 0xFFFFFFFF
+andi $a0, $t0, 0x5555
+jal print_a0
+nop
+
+li $t0, 0xFFFFFFFF
+andi $a0, $t0, 0xAAAA
+jal print_a0
+nop
+
+li $t0, 0xFFFFFFFF
+andi $a0, $t0, 0xFFFF
+jal print_a0
+nop
+
+j exit
+nop
+
+
+print_a0:
+li $v0, 1
+syscall
+
+li $v0, 11
+li $a0, 10
+syscall
+
+jr $ra
+nop
+
+exit:
+li $v0, 10
+li $a0, 0
+syscall
