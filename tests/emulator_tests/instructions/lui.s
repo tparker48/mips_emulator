08469cf5a31901986dabb0d3727095055f9ed63a
Thomas Parker <32755663+tparker48@users.noreply.github.com>
2025-11-16 17:43:35 -0800

    add lui and lw tets

--- tests/emulator_tests/instructions/lui.s
+++ tests/emulator_tests/instructions/lui.s
@@ -1,1 +1,41 @@
-# TODO
+# lui 0 = 0
+lui $a0, 0
+jal call_lui 
+nop
+
+# lui 1 = 65536
+lui $a0, 1
+jal call_lui 
+nop
+
+# lui 2 = 131072
+lui $a0, 2
+jal call_lui 
+nop
+
+# lui 0xFFFF = -65536
+lui $a0, 0xFFFF
+jal call_lui 
+nop
+
+# lui 0x8000 = -2147483648
+lui $a0, 0x8000
+jal call_lui 
+nop
+
+jal exit
+nop
+
+call_lui:
+li $v0, 1
+syscall
+li $v0, 11
+li $a0, 10
+syscall
+jr $ra
+nop
+
+exit:
+li $v0, 10
+li $a0, 0
+syscall
--- tests/emulator_tests/instructions/lw.s
+++ tests/emulator_tests/instructions/lw.s
@@ -1,1 +1,53 @@
-# TODO
+.text
+la $t0, test_byte
+
+lw $a0, 0($t0)
+li $v0, 1
+syscall
+li $a0, 10
+li $v0, 11
+syscall
+
+li $t1, 0x55
+sw $t1, 0($t0)
+lw $a0, 0($t0)
+li $v0, 1
+syscall
+li $a0, 10
+li $v0, 11
+syscall
+
+li $t1, 0xFF00
+sw $t1, 0($t0)
+lw $a0, 0($t0)
+li $v0, 1
+syscall
+li $a0, 10
+li $v0, 11
+syscall
+
+li $t1, 0xFFFF0000
+sw $t1, 0($t0)
+lw $a0, 0($t0)
+li $v0, 1
+syscall
+li $a0, 10
+li $v0, 11
+syscall
+
+li $t1, 0xFFFFFFFF
+sw $t1, 0($t0)
+lw $a0, 0($t0)
+li $v0, 1
+syscall
+li $a0, 10
+li $v0, 11
+syscall
+
+exit:
+li $a0, 0
+li $v0, 10
+syscall
+
+.data
+test_byte: .byte 0xAA
