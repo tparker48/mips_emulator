f0414894a6fe82f2454218fcce7d97cfd80cd120
Thomas Parker <32755663+tparker48@users.noreply.github.com>
2025-11-17 22:35:59 -0800

    more psuedo tests

--- tests/emulator_tests/pseudos/clear.s
+++ tests/emulator_tests/pseudos/clear.s
@@ -1,1 +1,28 @@
-# TODO
+li $a0, 0xFFFFFFFF
+clear $a0
+jal print_a0
+nop
+
+li $a0, 0xAAAAAAAA
+clear $a0
+jal print_a0
+nop
+
+li $a0, 0x00000001
+clear $a0
+jal print_a0
+nop
+
+exit:
+li $v0, 10
+li $a0, 0
+syscall
+
+print_a0:
+li $v0, 1
+syscall
+li $v0, 11
+li $a0, 10
+syscall
+jr $ra
+nop
--- tests/emulator_tests/pseudos/la.s
+++ tests/emulator_tests/pseudos/la.s
@@ -1,1 +1,27 @@
-# TODO
+.text
+nop
+nop
+text_addr: 
+nop
+
+la $a0, text_addr
+li $v0, 1
+syscall
+li $v0, 11
+li $a0, 10
+syscall
+
+la $a0, data_addr
+li $v0, 1
+syscall
+li $v0, 11
+li $a0, 10
+syscall
+
+li $v0, 10
+li $a0, 0
+syscall
+
+.data
+.word 0
+data_addr: .word 0
